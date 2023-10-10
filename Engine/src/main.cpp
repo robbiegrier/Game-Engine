a17df460fc1592ffa88d59f17d8a2a4b9a85255b
Robbie Grier <robbiegrier10@gmail.com>
2023-10-10 13:17:47 -0700

    add json header

--- Engine/src/main.cpp
+++ Engine/src/main.cpp
@@ -3,10 +3,20 @@
 #include "PCSNode.h"
 #include "PCSTreeForwardIterator.h"
 
+#include "json.hpp"
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPWSTR cmdLine, int cmdShow)
 {
 	static_cast<void>(prevInstance);
 	static_cast<void>(cmdLine);
+
+	nlohmann::json printer = {
+		{"Engine", true},
+		{"Version", 1.0},
+	};
+
+	Trace::out(printer.dump(4).c_str());
+	Trace::out("\n\n");
 
 	int output = Azul::Game::Launch(hInstance, cmdShow);
 	Azul::Game::Teardown();
